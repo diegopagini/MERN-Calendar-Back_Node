0ab1d087528df3db8afb4a4621d8fa635906bdff
Diego Pagini <dq@qbkconsulting.com>
2021-04-29 09:11:03 -0300

    mogodb

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 /** @format */
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
 	const { name, email, password } = req.body;
