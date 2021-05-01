5968a4c602791e30d7360497e563d9e93d64e551
Diego Pagini <dq@qbkconsulting.com>
2021-05-01 08:02:39 -0300

    generar un jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,36 @@
 /** @format */
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async (req, res = response) => {
-	// const { name, email, password } = req.body;
+	const { password, email } = req.body;
 
 	try {
-		const usuario = new Usuario(req.body);
+		let usuario = await Usuario.findOne({ email });
+		if (usuario) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Un usuario existe con ese correo',
+			});
+		}
+
+		usuario = new Usuario(req.body);
+
+		//Encriptar contraseña
+		const salt = bcrypt.genSaltSync();
+		usuario.password = bcrypt.hashSync(password, salt);
 
 		await usuario.save();
+		// Generar JWT
+		const token = await generarJWT(usuario.id, usuario.name);
 
 		res.status(201).json({
 			ok: true,
-			msg: 'Register',
+			uid: usuario.id,
+			name: usuario.name,
+			token,
 		});
 	} catch (error) {
 		console.log(error);
@@ -23,15 +41,44 @@ const crearUsuario = async (req, res = response) => {
 	}
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
 	const { email, password } = req.body;
 
-	res.status(201).json({
-		ok: true,
-		msg: 'Login',
-		email,
-		password,
-	});
+	try {
+		const usuario = await Usuario.findOne({ email });
+		if (!usuario) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Un usuario existe con ese correo',
+			});
+		}
+
+		// Confirmar los passwords
+		const validPassword = bcrypt.compareSync(password, usuario.password);
+
+		if (!validPassword) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Password incorrecto',
+			});
+		}
+
+		// Generar nuestro JWT
+		const token = await generarJWT(usuario.id, usuario.name);
+
+		res.json({
+			ok: true,
+			uid: usuario.id,
+			name: usuario.name,
+			token,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			mgs: 'Por favor hable con el administrador',
+		});
+	}
 };
 
 const revalidarToken = (req, res = response) => {
